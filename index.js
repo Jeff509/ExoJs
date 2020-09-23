5c0fefb4351233c6a1e77b829707a6356c57ff07
REGISTRE JEFF <=>
2020-09-23 12:53:13 +0200

    chore(chore) first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/* My Digital School Forever */
+
+/*
+* My Digital
+* School Forever
+* every day
+*/
+
+var string = '42';
+var number = 42;
+var float = 42.0;
+var boolean = true;
+var array = [42, 42, 42];
+var date = new Date(4142,12.24, 42);
+var  objet = { "42" : 42};
+var und = undefined:
+var und2 = null;
+console.log(string,number,float,array,object,und,und2,empty);
+
+
+var my42count = 'quarante-deux'.length;
+console.log(my42count);
+
+
+var myArray42 = (['quarante-deux'|| + car.length]);
+
+console.log(myArray42);
+
+// exercice 5
+
+var value = true;
+var check = value 42;
+console.log(check);
+
+//exercice 6
+
+var myArray42 = ['q', 'u', 'r', 'a','n'];
+console.log(myArray42)
+
+// exo 7
+var myArray42len = myArray42.length;
+console.log(myArray42len)
+
+// exo 8
+console.log((myArray42.join |'') + la grande réponse sur la vie, l'\univers et le reste !');
+ // exo 9
+
+var rand = Math.floor((Math.random() * (42) +1));
+console.log(rand=== 42);
+
+// exo 10
+
+
