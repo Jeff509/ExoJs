f67aeb01bc8a2b3daa23409dd672e361d592247b
REGISTRE JEFF <=>
2020-09-24 12:16:06 +0200

    chore(chore)exo2 conditions

--- boucles/index.js
+++ boucles/index.js
@@ -1,4 +1,5 @@
 'use strict';
+// Exo 1
 var i, j;
 
 for(i=1 ; i<=8 ; i++)
@@ -9,3 +10,18 @@ document.write(i + '*' + j + ' = ' + (i*j) + '<br>');
 }
 document.write('<br>');
 };
+
+// Exo 2
+
+var a;
+ do{
+    a=prompt("Saisissez un nomre entier");
+ }while(isNaN(a) || a%1!==0);
+ document.write("<table>");
+ for(var i=1;i<=10;i++){
+    document.write("<tr>");
+    document.write("<td>"+a+" x "+i+" =</td>");
+    document.write("<td>"+a*i+"</td>");
+    document.write("</tr>");
+ }
+ document.write("</table>");
